0d1ff99e43ed9260f2f0db4b1b7bb947a34f2393
helen <nhelennn11@gmail.com>
2022-06-15 20:17:51 +0700

    integrate

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom/client';
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
-
-const root = document.getElementById("root");
-render(
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>,
-  root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+  </Provider>
 );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,49 @@
-import { Form, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import "../css/main.css";
-
+import React, { useRef, useState } from "react";
+import { Form, Row, Col, Button, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Register() {
+    const navigate = useNavigate();
+
+    const nameField = useRef("");
+    const emailField = useRef("");
+    const passwordField = useRef("");
+
+    const [errorResponse, setErrorResponse] = useState({
+        isError: false,
+        message: "",
+    });
+
+    const onRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userToRegisterPayload = {
+                name: nameField.current.value,
+                email: emailField.current.value,
+                password: passwordField.current.value,
+            };
+
+            const registerRequest = await axios.post(
+                "http://localhost:8888/auth/register",
+                userToRegisterPayload
+            );
+
+            const registerResponse = registerRequest.data;
+
+            if (registerResponse.status) navigate("/login");
+        } catch (err) {
+            console.log(err);
+            const response = err.response.data;
+
+            setErrorResponse({
+                isError: true,
+                message: response.message,
+            });
+        }
+    };
 
     const styleLabel = {
         borderRadius: '10px',
@@ -23,11 +63,12 @@ export default function Register() {
                 </Col>
                 <Col className="register-right">
                     <h3 className="mb-3">Daftar</h3>
-                    <Form >
+                    <Form onSubmit={onRegister}>
                         <Form.Group className="mb-3">
                             <Form.Label>Nama</Form.Label>
                             <Form.Control
                                 type="text"
+                                ref={nameField}
                                 placeholder="Nama Lengkap"
                                 style={styleLabel}
                             />
@@ -36,6 +77,7 @@ export default function Register() {
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 type="text"
+                                ref={emailField}
                                 placeholder="Contoh: johndee@gmail.com"
                                 style={styleLabel}
                             />
@@ -44,10 +86,14 @@ export default function Register() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control
                                 type="password"
+                                ref={passwordField}
                                 placeholder="Masukkan Password"
                                 style={styleLabel}
                             />
                         </Form.Group>
+                        {errorResponse.isError && (
+                            <Alert variant="danger">{errorResponse.message}</Alert>
+                        )}
                         <Button className="w-100" type="submit" style={styleLabel}>
                             Daftar
                         </Button>
