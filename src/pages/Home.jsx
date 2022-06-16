f5613303e46143935e53a58bfcdab6810a6040e4
leonardomartine <leonardomartine1508@gmail.com>
2022-06-16 20:34:45 +0700

    login

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import "../css/main.css";
+
+
+export default function Home() {
+
+    return (
+        <>
+         <Header />
+
+        </>
+    );
+}
